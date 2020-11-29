cc0e3b5273d3e4245dc5d7594be9be8c850dc7f6
Samara Grant <samara.amanda01@gmail.com>
2020-11-29 07:33:51 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ require 'pry'
 require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
